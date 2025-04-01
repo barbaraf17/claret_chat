e1ae8683856b0c833fded7b5074cbff677f0c2c5
barbaraf17 <vicky.b@integro.co.in>
2025-04-01 11:10:07 +0530

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'standalone',
-  images: {
-    domains: ['chat1.claretcollege.edu.in'],
-  },
-  // Ensure proper handling of environment variables
-  env: {
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-  },
-
- eslint: {
-    ignoreDuringBuilds: true,
-  }
-};
+const nextConfig = {};
 
 export default nextConfig;
